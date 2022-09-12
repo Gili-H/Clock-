a210e3aa58ab9ddde2a8f22f79a7a8c31fee625b
Gili-H <gilih3479@gmailc.com>
2022-09-12 20:18:35 +0300

    Addition of a script which check if the hand is located in the right place

---
+++ Clock/Assets/Scripts/Succes_Check.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Succes_Check : MonoBehaviour
+{
+    //[SerializeField]
+    //private float angle;
+
+    //[SerializeField]
+    //private GameObject gameObject;
+    //private Transform transform;
+    
+    //private Display_Time display_Time;
+    //private int hour;
+
+    //private void Start()
+    //{
+    //    transform = gameObject.GetComponent<Transform>();
+    //    display_Time = gameObject.GetComponent<Display_Time>();
+    //    hour = display_Time.hour;
+    //}
+
+    //private void Update()
+    //{
+    //    angle = transform.rotation.eulerAngles.z;
+    //    int a = (int)angle;
+    //    float w = Mathf.Abs(a) / 30f;
+
+    //    if (w == hour)
+    //    {
+    //        Debug.Log("Succeed");
+    //    }
+
+    //}
+}
